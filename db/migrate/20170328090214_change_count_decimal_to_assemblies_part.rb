287c0a632876b996b7a077c4a89c15a82f66bd9b
julien-leclercq <julien.leclercq@derniercri.io>
2017-03-28 11:09:30 +0200

    fix table name in last commit

--- db/migrate/20170328090214_change_count_decimal_to_assemblies_part.rb
+++ db/migrate/20170328090214_change_count_decimal_to_assemblies_part.rb
@@ -1,5 +1,5 @@
 class ChangeCountDecimalToAssembliesPart < ActiveRecord::Migration[5.0]
   def change
-    change_column :spree_assemblies_part, :count, :decimal, precision: 8, scale: 5
+    change_column :spree_assemblies_parts, :count, :decimal, precision: 8, scale: 5
   end
 end
