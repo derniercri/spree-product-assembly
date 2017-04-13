7d606d12bbba9b317488c2471b921c95741bf23e
julien-leclercq <julien.leclercq@derniercri.io>
2017-04-13 18:35:51 +0200

    fixed typo

--- db/migrate/20170413140555_add_quantity_to_inventory_units.rb
+++ db/migrate/20170413140555_add_quantity_to_inventory_units.rb
@@ -1,5 +1,5 @@
 class AddQuantityToInventoryUnits < ActiveRecord::Migration[5.0]
   def change
-    add_column :spree_inventory_units, :quantity, :decimal, presicion: 8, scale: 5
+    add_column :spree_inventory_units, :quantity, :decimal, precision: 8, scale: 5
   end
 end
